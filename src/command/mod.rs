206624c9c8a46a32ffa1da67651fe907f0087154
brkp <brkp@tutanota.com>
2022-07-30 20:01:52 +0300

    type alias

--- src/command/mod.rs
+++ src/command/mod.rs
@@ -1,3 +1,6 @@
 pub mod device;
 
-type CmdFn = Box<dyn Fn(&mut Vec<u8>) -> usize>;
+/// A closure with the type `CmdFn` is returned from each command generator
+/// function so that some other function can call this closure to build the
+/// command data in-place, without knowing anything about the command.
+pub type CmdFn = Box<dyn Fn(&mut Vec<u8>) -> usize>;
